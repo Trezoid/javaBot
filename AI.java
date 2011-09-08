b083d197ab1a6fc4c73006e1e1440a1e3d973c44
Liam Davis <liam.davis@internode.on.net>
2011-09-08 19:34:22 +1000

    Fixed local AI

--- AI.java
+++ AI.java
@@ -35,7 +35,7 @@ public class AI
 			String line;
 			while((line = br.readLine()) != null)
 			{
-				String[] resMap = line.split("|");
+				String[] resMap = line.split("=");
 				res.put(resMap[0], resMap[1]);
 			}
 		}
@@ -47,40 +47,34 @@ public class AI
 
 	public String ai(String message, String sender)
 	{
-		 String aiMessage;
-
+		String[] in;
+		String aiMessage;
 		if(localAI == true)
 		{
-			if((aiMessage = res.get(message.split(botName+": ")[1])) != null)
-			{
-				return aiMessage;
+			try{
+				if(res.containsKey(message.toLowerCase()))
+				{
+					return res.get(message.toLowerCase());
+				}
+				in = message.toLowerCase().split("\\p{Punct}");
+				for(int i = 0; i < in.length; i++)
+				 {
+					 if(res.containsKey(in[i].toLowerCase()))
+					 {
+						 return res.get(in[i].toLowerCase());
+					 }
+				 }
+
+				in = message.toLowerCase().split(" ");
+				for(int i = 0; i < in.length; i++)
+				{
+					if(res.containsKey(in[i].toLowerCase().replaceAll("\\p{Punct}", "")))
+					{
+						return res.get(in[i].toLowerCase().replaceAll("\\p{Punct}", ""));
+					}
+				}
 			}
-			String[] sentence = message.toLowerCase().split(".");
-			for(int i = 1; i < sentence.length; i++)
-			 {
-				 if((aiMessage = res.get(sentence[i]))!= null)
-				 {
-					 return aiMessage;
-				 }
-			 }
-			String[] question = message.toLowerCase().split("?");
-			for(int i = 1; i < question.length; i++)
-			 {
-				 if((aiMessage = res.get(question[i]+"?"))!= null)
-				 {
-					 return aiMessage;
-				 }
-			 }
-
-
-			String[] in = message.toLowerCase().split(" ");
-			for(int i = 1; i < in.length; i++)
-			 {
-				 if((aiMessage = res.get(in[i]))!= null)
-				 {
-					 return aiMessage;
-				 }
-			 }
+			catch(Exception e){System.out.println("4"+e.getMessage());}
 		}
 		String result =  "";
 		try{ 
@@ -109,7 +103,7 @@ public class AI
 		try{
 		String splitResult = result.split("<h2>")[1];
 		aiMessage = splitResult.split("</h2>")[0];
-		aiMessage = aiMessage.replace("&#8217;", "'").replace("&#8221;", "\"").replace("&#8220;", "\"").replace("#&8212;", "-").replace("&lt;br/&gt;", "\n").replace("Kato", botName).replace("undefined", sender);
+		aiMessage = aiMessage.replace("&#8217;", "'").replace("&#8221;", "\"").replace("&#8220;", "\"").replace("#&8212;", "-").replace("&lt;br/&gt;", "\n").replace("Kato", botName).replace("undefined", sender).replace("&#8230;", "…");
 		return aiMessage;
 		}
 	catch(Exception e){System.out.println(e.getMessage());return "My brain seems to be asleep. Sorry about that.";}
